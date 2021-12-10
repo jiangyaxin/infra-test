b57ce08268dd1888bd338937a41afe1d876a54c8
jiangyaxin <jiangyaxin@sutpc.com>
2021-12-10 20:27:11 +0800

    新增oauth2-client

---
+++ feature/test-oauth2/test-login/src/main/java/com/jyx/feature/test/oauth2/login/LoginApplication.java
@@ -0,0 +1,20 @@
+package com.jyx.feature.test.oauth2.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author JYX
+ * @since 2021/10/13 16:00
+ */
+@SpringBootApplication
+@ComponentScan("com.jyx")
+public class LoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginApplication.class, args);
+    }
+
+}
+
---
+++ feature/test-oauth2/test-login/src/main/java/com/jyx/feature/test/oauth2/login/config/Oauth2LoginConfig.java
@@ -0,0 +1,39 @@
+package com.jyx.feature.test.oauth2.login.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * WebAsyncManagerIntegrationFilter
+ * SecurityContextPersistenceFilter
+ * HeaderWriterFilter
+ * LogoutFilter
+ * OAuth2AuthorizationRequestRedirectFilter
+ * OAuth2LoginAuthenticationFilter
+ * DefaultLoginPageGeneratingFilter
+ * DefaultLogoutPageGeneratingFilter
+ * RequestCacheAwareFilter
+ * SecurityContextHolderAwareRequestFilter
+ * AnonymousAuthenticationFilter
+ * SessionManagementFilter
+ * ExceptionTranslationFilter
+ * FilterSecurityInterceptor
+ *
+ * @author JYX
+ * @since 2021/11/24 16:45
+ */
+@Configuration
+public class Oauth2LoginConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .antMatcher("/**").authorizeRequests()
+                .antMatchers("/index.html").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login();
+    }
+}
