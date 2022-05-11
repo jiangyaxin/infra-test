214e3d9c5a6071dee037853abdda16892ea778d2
jiangyaxin <792124098@qq.com>
2022-05-12 01:56:33 +0800

    调整 AuthorizationServer 使用 UTC 时间

--- feature/test-oauth2/test-login/src/main/java/com/jyx/feature/test/oauth2/login/config/Oauth2LoginConfig.java
+++ feature/test-oauth2/test-login/src/main/java/com/jyx/feature/test/oauth2/login/config/Oauth2LoginConfig.java
@@ -2,6 +2,7 @@ package com.jyx.feature.test.oauth2.login.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -55,5 +56,10 @@ public class Oauth2LoginConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .oauth2Login();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/favicon.ico");
     }
 }
