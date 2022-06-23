1f6690d3b0336bdf27d516dd1bb0892a76828f4d
jiangyaxin <jiangyaxin@sutpc.com>
2022-06-23 09:47:18 +0800

    调整 GlobalApplicationEvent

--- infra-bus/src/main/java/com/jyx/infra/bus/event/GlobalApplicationEvent.java
+++ infra-bus/src/main/java/com/jyx/infra/bus/event/GlobalApplicationEvent.java
@@ -2,6 +2,7 @@ package com.jyx.infra.bus.event;
 
 import lombok.Getter;
 import lombok.ToString;
+import org.springframework.cloud.bus.event.Destination;
 import org.springframework.cloud.bus.event.RemoteApplicationEvent;
 
 import java.util.Date;
@@ -34,15 +35,15 @@ public abstract class GlobalApplicationEvent extends RemoteApplicationEvent {
      * 例如：
      *      @Autowired private ServiceMatcher serviceMatcher
      *      serviceMatcher.getServiceId()
-     * @param destinationService 事件目的地
+     * @param destination 事件目的地
      * 样式：  spring.application.name : port : 随机字符串
      * 例如: bus-kafka:30003:9025e7977392de95a3e70249810f924a
      * 如果广播给所有服务的所有实例，则设置为 **
      * 如果广播给 bus-kafka 服务的所有实例，则设置为 bus-kafka:**
      * 如果广播给 bus-kafka 服务的指定实例，则设置为 bus-kafka:30003:9025e7977392de95a3e70249810f924a
      */
-    public GlobalApplicationEvent(Object source, String originService, String destinationService) {
-        super(source, originService, destinationService);
+    public GlobalApplicationEvent(Object source, String originService, Destination destination) {
+        super(source, originService, destination);
         init();
     }
 
@@ -52,7 +53,7 @@ public abstract class GlobalApplicationEvent extends RemoteApplicationEvent {
      * @param originService 事件发送源
      */
     public GlobalApplicationEvent(Object source, String originService) {
-        super(source, originService);
+        super(source, originService,DEFAULT_DESTINATION_FACTORY.getDestination(null));
         init();
     }
 
