5d2cf108118eae255124839da19180e936105248
DESKTOP-A08CFU0\Archforce <jiangyaxin@archforce.cn>
2022-10-18 11:19:49 +0800

    新增 SPEL 单元测试

---
+++ feature/test-jpa/src/test/java/com/jyx/feature/test/jpa/SpelTest.java
@@ -0,0 +1,20 @@
+package com.jyx.feature.test.jpa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+
+/**
+ * @author Archforce
+ * @since 2022/10/14 15:36
+ */
+public class SpelTest {
+
+    @Test
+    public void numberPlusTest(){
+
+        SpelExpressionParser parser = new SpelExpressionParser();
+        Integer value = parser.parseExpression("(21+3)*4").getValue(Integer.class);
+        Assertions.assertEquals((21+3)*4,value);
+    }
+}
