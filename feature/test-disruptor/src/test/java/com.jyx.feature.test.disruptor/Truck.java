d48dffc3b4f3f3517147d78a08ca0f5dfad2efef
DESKTOP-A08CFU0\Archforce <jiangyaxin@archforce.cn>
2022-12-02 20:55:39 +0800

    新增 disruptor 测试

---
+++ feature/test-disruptor/src/test/java/com.jyx.feature.test.disruptor/Truck.java
@@ -0,0 +1,23 @@
+package com.jyx.feature.test.disruptor;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Archforce
+ * @since 2022/12/1 9:57
+ */
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Truck<T> {
+
+    private T data;
+
+    public void load(T data) {
+        this.data = data;
+    }
+}
