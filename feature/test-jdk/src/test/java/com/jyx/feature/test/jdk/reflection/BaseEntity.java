6b6479d0556e71d3e3560532ac7fa575c26d031d
DESKTOP-A08CFU0\Archforce <jiangyaxin@archforce.cn>
2023-01-31 15:21:00 +0800

    新增jdk测试

---
+++ feature/test-jdk/src/test/java/com/jyx/feature/test/jdk/reflection/BaseEntity.java
@@ -0,0 +1,14 @@
+package com.jyx.feature.test.jdk.reflection;
+
+import java.util.Date;
+
+/**
+ * @author Archforce
+ * @since 2023/1/20
+ */
+public class BaseEntity {
+
+    public Date timeAdd;
+
+    public Date timeMod;
+}
