b35cc88032564e61c43f0cec513130367e6bd57a
jiangyaxin <792124098@qq.com>
2023-03-01 23:17:46 +0800

    继承AutoClose的Bean会被Spring关闭

--- infra-spring/src/main/java/com/jyx/infra/spring/config/IdConfig.java
+++ infra-spring/src/main/java/com/jyx/infra/spring/config/IdConfig.java
@@ -2,17 +2,11 @@ package com.jyx.infra.spring.config;
 
 import com.jyx.infra.id.IdAllocators;
 import com.jyx.infra.id.SnowflakeIdAllocator;
-import com.jyx.infra.spring.context.AppConstant;
 import com.jyx.infra.spring.context.AppContext;
 import com.jyx.infra.spring.context.ClusterContext;
-import com.jyx.infra.spring.id.SnowflakeIdAllocatorWrapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
-
-import javax.annotation.PreDestroy;
-import java.util.Optional;
 
 /**
  * @author JYX
@@ -32,9 +26,9 @@ public class IdConfig {
 
         boolean enabledCachedIdGenerator = appContext.getCluster().isEnabledCachedIdGenerator();
         if (enabledCachedIdGenerator) {
-            return new SnowflakeIdAllocatorWrapper<>(IdAllocators.ofCachedSnowflake(dataCenterId, workerId));
+            return IdAllocators.ofCachedSnowflake(dataCenterId, workerId);
         } else {
-            return new SnowflakeIdAllocatorWrapper<>(IdAllocators.ofDefaultSnowflake(dataCenterId, workerId));
+            return IdAllocators.ofDefaultSnowflake(dataCenterId, workerId);
         }
     }
 }
