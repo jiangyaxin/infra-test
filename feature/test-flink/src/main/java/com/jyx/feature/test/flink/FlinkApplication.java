a51ed3e348aef12e58e9ab0c525a37bf42355c53
DESKTOP-A08CFU0\Archforce <jiangyaxin@archforce.cn>
2023-03-23 18:08:23 +0800

    新增Flink

---
+++ feature/test-flink/src/main/java/com/jyx/feature/test/flink/FlinkApplication.java
@@ -0,0 +1,16 @@
+package com.jyx.feature.test.flink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author asa
+ */
+@SpringBootApplication
+public class FlinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlinkApplication.class, args);
+    }
+
+}
