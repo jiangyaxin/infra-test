22d264cbff720ae4d44d9dd508885938402aeb82
DESKTOP-A08CFU0\Archforce <jiangyaxin@archforce.cn>
2023-03-24 15:23:49 +0800

    新增flink

---
+++ feature/test-flink/src/main/java/com/jyx/feature/test/flink/FlinkApplication.java
@@ -0,0 +1,15 @@
+package com.jyx.feature.test.flink;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author asa
+ */
+@Slf4j
+public class FlinkApplication {
+
+    public static void main(String[] args) {
+
+    }
+
+}
