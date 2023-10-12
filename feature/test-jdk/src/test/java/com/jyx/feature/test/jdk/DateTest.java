f8e9f0af0b8698609b67b666d63c9fdc07723025
DESKTOP-A08CFU0\Archforce <jiangyaxin@archforce.cn>
2023-10-12 14:05:16 +0800

    新增 日期 测试

---
+++ feature/test-jdk/src/test/java/com/jyx/feature/test/jdk/DateTest.java
@@ -0,0 +1,26 @@
+package com.jyx.feature.test.jdk;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @author Archforce
+ * @since 2023/10/12 11:26
+ */
+@Slf4j
+public class DateTest {
+
+    @Test
+    public void localDateTest() {
+        DateTimeFormatter yyyyMMdd = DateTimeFormatter.ofPattern("yyyyMMdd");
+        LocalDate localDate = LocalDate.parse("20230705", yyyyMMdd);
+        int year = localDate.getYear();
+        int monthValue = localDate.getMonthValue();
+        int dayOfMonth = localDate.getDayOfMonth();
+
+        log.info(year + "-" + monthValue + "-" + dayOfMonth);
+    }
+}
