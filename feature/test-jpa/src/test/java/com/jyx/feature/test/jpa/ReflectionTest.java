9eca32ae98c7f50c3f9b8474ed478b9c3cba6ef6
DESKTOP-A08CFU0\Archforce <jiangyaxin@archforce.cn>
2023-10-12 11:13:20 +0800

    新增 Reflections 测试

--- feature/test-jpa/src/test/java/com/jyx/feature/test/jpa/ReflectionTest.java
+++ feature/test-jpa/src/test/java/com/jyx/feature/test/jpa/ReflectionTest.java
@@ -1,8 +1,15 @@
 package com.jyx.feature.test.jpa;
 
 import com.jyx.feature.test.jpa.domain.service.LightGroupService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.core.type.classreading.CachingMetadataReaderFactory;
+import org.springframework.core.type.classreading.MetadataReader;
+import org.springframework.core.type.classreading.MetadataReaderFactory;
 
 import java.util.Set;
 
@@ -10,6 +17,7 @@ import java.util.Set;
  * @author Archforce
  * @since 2023/10/11 17:36
  */
+@Slf4j
 public class ReflectionTest {
 
     @Test
@@ -18,4 +26,41 @@ public class ReflectionTest {
         Set<Class<? extends LightGroupService>> subTypesOf = reflections.getSubTypesOf(LightGroupService.class);
         System.out.println(1);
     }
+
+    @Test
+    public void springPackageScan() {
+        ResourcePatternResolver resourcePatternResolver = new PathMatchingResourcePatternResolver();
+        String pattern = ResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX + "/com/jyx/**/*.class";
+        Resource[] resources = getResources(resourcePatternResolver, pattern);
+        MetadataReaderFactory readerFactory = new CachingMetadataReaderFactory(resourcePatternResolver);
+
+        for (Resource resource : resources) {
+            MetadataReader reader = getMetadataReader(readerFactory, resource);
+            String classname = reader.getClassMetadata().getClassName();
+            try {
+                Class<?> clazz = Class.forName(classname);
+                log.info(clazz.getName());
+            } catch (Throwable e) {
+                log.error("", e);
+            }
+        }
+
+    }
+
+    private Resource[] getResources(ResourcePatternResolver resourcePatternResolver, String pattern) {
+        try {
+            return resourcePatternResolver.getResources(pattern);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private MetadataReader getMetadataReader(MetadataReaderFactory readerFactory, Resource resource) {
+        try {
+            return readerFactory.getMetadataReader(resource);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
