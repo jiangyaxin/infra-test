28c8196a0947fcbaa53083e5724ce02d6d470fa7
DESKTOP-A08CFU0\Archforce <jiangyaxin@archforce.cn>
2023-10-20 17:42:51 +0800

    新增 toolkit

---
+++ feature/toolkit/src/main/java/com/jyx/feature/toolkit/ToolkitApplication.java
@@ -0,0 +1,13 @@
+package com.jyx.feature.toolkit;
+
+/**
+ * @author JYX
+ * @since 2021/10/13 16:00
+ */
+public class ToolkitApplication {
+
+    public static void main(String[] args) {
+    }
+
+}
+
