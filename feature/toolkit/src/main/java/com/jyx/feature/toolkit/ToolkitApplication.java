5308d1bf7eaea10118d354ea540a69d50bf1f1bf
DESKTOP-A08CFU0\Archforce <jiangyaxin@archforce.cn>
2023-10-20 18:57:23 +0800

    新增 toolkit

--- feature/toolkit/src/main/java/com/jyx/feature/toolkit/ToolkitApplication.java
+++ feature/toolkit/src/main/java/com/jyx/feature/toolkit/ToolkitApplication.java
@@ -1,12 +1,17 @@
 package com.jyx.feature.toolkit;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author JYX
  * @since 2021/10/13 16:00
  */
+@SpringBootApplication
 public class ToolkitApplication {
 
     public static void main(String[] args) {
+        SpringApplication.run(ToolkitApplication.class, args);
     }
 
 }
