dd521898765eca81884afbb56a8d73c9a40249ef
DESKTOP-A08CFU0\Archforce <jiangyaxin@archforce.cn>
2023-11-04 16:41:19 +0800

    新增Pipeline

--- feature/test-mybatis-plus/src/main/java/com/jyx/feature/test/mybatis/plus/pipeline/LogPipeline.java
+++ feature/test-mybatis-plus/src/main/java/com/jyx/feature/test/mybatis/plus/pipeline/LogPipeline.java
@@ -1,7 +1,9 @@
 package com.jyx.feature.test.mybatis.plus.pipeline;
 
+import com.jyx.feature.test.mybatis.plus.repository.repo1.service.StageService;
 import com.jyx.infra.spring.pipeline.Pipeline;
 import com.jyx.infra.spring.pipeline.Stage;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -10,11 +12,14 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 @Pipeline
+@RequiredArgsConstructor
 public class LogPipeline {
+
+    private final StageService stageService;
 
     @Stage(order = 1)
     public void stage1(Integer param) {
-        log.info("阶段1 消费 {}", param);
+        log.info("阶段1 消费 {}", stageService.getById(param));
     }
 
     @Stage(order = 2)
