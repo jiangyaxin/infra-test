eb52435c958918e67e3a456013ba73e36c80b06c
DESKTOP-A08CFU0\Archforce <jiangyaxin@archforce.cn>
2023-11-29 18:17:44 +0800

    新增 Excel

--- infra/src/main/java/com/jyx/infra/util/ExcelUtil.java
+++ infra/src/main/java/com/jyx/infra/util/ExcelUtil.java
@@ -1,8 +1,11 @@
 package com.jyx.infra.util;
 
 import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.ExcelReader;
 import com.alibaba.excel.ExcelWriter;
+import com.alibaba.excel.read.metadata.ReadSheet;
 import com.alibaba.excel.write.metadata.WriteSheet;
+import com.jyx.infra.excel.AsyncExcelReadListener;
 import com.jyx.infra.exception.ExcelException;
 
 import java.io.Closeable;
@@ -11,12 +14,31 @@ import java.io.OutputStream;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.function.Function;
 
 /**
  * @author Archforce
  * @since 2023/11/28 17:49
  */
 public class ExcelUtil {
+
+    interface Constants {
+        int TASK_SIZE_OF_EACH_WORKER = 5000;
+        int EXCEPTED_TASK_SIZE = 512;
+    }
+
+    public static <T, OUT> List<OUT> readExcel(String path, Class<T> clazz,
+                                               Function<List<T>, OUT> dataProcessor, ExecutorService executorService) {
+        AsyncExcelReadListener<T, OUT> readListener = new AsyncExcelReadListener<>(executorService, Constants.TASK_SIZE_OF_EACH_WORKER, Constants.EXCEPTED_TASK_SIZE, dataProcessor);
+        try (ExcelReader excelReader = EasyExcel.read(path, clazz, readListener).build()) {
+            List<ReadSheet> readSheetList = excelReader.excelExecutor().sheetList();
+            for (ReadSheet readSheet : readSheetList) {
+                excelReader.read(readSheet);
+            }
+        }
+        return readListener.getResult();
+    }
 
     public static <T> String writeExcelToPath(String path, List<T> dataList, String sheetName,
                                               Class<?> excelClazz, Collection<String> includeFieldNameCollection) {
