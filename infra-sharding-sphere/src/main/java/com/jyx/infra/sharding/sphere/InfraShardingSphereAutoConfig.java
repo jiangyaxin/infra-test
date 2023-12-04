204767bdbb8e800170dcf493d1b0aed0bb524d93
DESKTOP-A08CFU0\Archforce <jiangyaxin@archforce.cn>
2023-12-04 14:01:40 +0800

    新增Infra-sharding-sphere

---
+++ infra-sharding-sphere/src/main/java/com/jyx/infra/sharding/sphere/InfraShardingSphereAutoConfig.java
@@ -0,0 +1,11 @@
+package com.jyx.infra.sharding.sphere;
+
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author Archforce
+ * @since 2023/12/4 13:54
+ */
+@ComponentScan("com.jyx.infra.sharding.sphere")
+public class InfraShardingSphereAutoConfig {
+}
